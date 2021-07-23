70ea0689f7b074986f2d558360f08b10b73729ce
ghofrandayyat <ghofrandayyat@gmail.com>
2021-07-23 14:54:51 +0300

    add product routes

--- src/models/products.js
+++ src/models/products.js
@@ -1,17 +1,18 @@
-'use strict'
+'use strict';
 
 const mongoose = require('mongoose');
 
 const productsSchema = mongoose.Schema({
-    name: {type: String, require: true},
-    image:  {type: String, require: true},
-    category: {type: String, require: true},
-    price: {type: Number},
-    phone: {type: String, require: true},
-    description: {type: String},
-    quantity: {type: Number},
-    owner: {type: String},
-    date: {type: Date, default: Date.now}
-})
+  name: {type: String, require: true},
+  image:  {type: String, require: true},
+  category: {type: String, require: true},
+  price: {type: Number},
+  phone: {type: String, require: true},
+  description: {type: String},
+  quantity: {type: Number},
+  owner: {type: String},
+  date: {type: Date, default: Date.now}
+});
 
-module.exports = productsModel = mongoose.model('product', productsSchema)
+const productsModel = mongoose.model('product', productsSchema);
+module.exports = productsModel;
--- src/server.js
+++ src/server.js
@@ -8,7 +8,8 @@ const cors = require('cors');
 app.use(cors());
 
 // call routes
-const authRoutes = require('../src/routers/auth')
+const authRouter = require('../src/routers/auth');
+const productRouter = require('../src/routers/product');
 
 // error handlers function
 const notFoundPage = require('../src/error-handlers/404');
@@ -18,27 +19,28 @@ const errorHandler = require('../src/error-handlers/500');
 app.use(express.json());
 // Process FORM input and put the data on req.body
 app.use(express.urlencoded({ extended: true }));
-// require the routes
-app.use(authRoutes)
 
+// require the routes
+// app.use(authRouter);
+app.use(productRouter);
 
 // error middleware
 app.use('*', notFoundPage);
 app.use(errorHandler);
 
 const start = (PORT, MONGODB_URI) => {
-    mongoose.connect(MONGODB_URI, {
-        useCreateIndex:true,
-        useFindAndModify: false,
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    }).then(() => {
-        console.log('connect to mongo ...');
-        app.listen( PORT,  () => console.log(`Listening To PORT ${PORT} ...`));
-    })
-}
+  mongoose.connect(MONGODB_URI, {
+    useCreateIndex:true,
+    useFindAndModify: false,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+  }).then(() => {
+    console.log('connect to mongo ...');
+    app.listen( PORT, () => console.log(`Listening To PORT ${PORT} ...`));
+  });
+};
 
 module.exports = {
-    start,
-    app
-}
+  start,
+  app
+};
